5131e0d7e73cae3a20f059dd41b562f1a52a620e
AntaMory <antamoryfall@gmail.com>
2023-12-13 03:11:46 +0100

    text

---
+++ Restaurant/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from rest_framework.decorators import permission_classes
+from rest_framework.response import Response
+from .models import Booking, Menu
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html", {})
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
